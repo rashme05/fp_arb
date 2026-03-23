69237d460a81738c02dc3406e8d64b46b9695892
Rashme <rasme.rathod@gmail.com>
2026-03-23 16:26:52 +0000

    test

---
+++ golden/arb.sv
@@ -0,0 +1,17 @@
+`timescale 1us/1us
+
+module arbiter_fp (
+    input  logic        clk,
+    input  logic [3:0]  req,
+    output logic [3:0]  grant
+);
+
+always_ff @(posedge clk) begin
+    if (req[3])       grant <= 4'b1000;
+    else if (req[2])  grant <= 4'b0100;
+    else if (req[1])  grant <= 4'b0010;
+    else if (req[0])  grant <= 4'b0001;
+    else              grant <= 4'b0000;
+end
+
+endmodule
